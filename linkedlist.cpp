dfa885981216890e474750cc379940ca8deb3994
Collin Campbell <ccampbell97@csu.fullerton.edu>
2019-05-02 21:04:36 -0700

    First commit

---
+++ linkedlist.cpp
@@ -0,0 +1,50 @@
+#include "linkedlist.h"
+
+linkedlist::linkedlist()
+{
+  head->data = 0;
+  head->next = nullptr;
+}
+
+linkedlist::~linkedlist()
+{
+    // RECURSIVE
+}
+
+int linkedlist::count()
+{
+    // RECURSIVE
+}
+
+void linkedlist::addToFront(int n)
+{
+  node *temp = head;
+  head = new node;
+  head->data = n;
+  head->next = temp;
+}
+
+double linkedlist::average()
+{
+    // uses sum and count functions
+}
+
+int linkedlist::sum()
+{
+    /*int data;
+    // RECURSIVE
+    if(head == nullptr)
+      return 0;
+    return head->data + head->next;*/
+
+}
+
+void linkedlist::writeInorder(string &file)
+{
+
+}
+
+void linkedlist::writeReversed(string &file)
+{
+    // RECURSIVE
+}
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "linkedlist.cpp"
+
+using namespace std;
+
+void readFile(linkedlist &list, string &file)
+{
+  ifstream inputFile;
+  int num;
+  inputFile.open(file);
+  inputFile >> num;
+  while(!inputFile.eof())
+  {
+    list.addToFront(num);
+    inputFile >> num;
+  }
+}
+
+int main()
+{
+    linkedlist *list;
+    string file = "input.txt";
+    readFile(list, file);
+    return 0;
+}
